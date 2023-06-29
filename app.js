7173184adaea2e250243267d18825ebda54dfc73
faycal-saadi <fayssoff@gmail.com>
2023-06-29 11:06:56 +0200

    Changement du message d'erreur en cas de 404

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'coffee');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,7 +28,8 @@ app.use('/groups', groups);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+ var err = new Error('That is not found');
+ next(createError(404));
 });
 
 // error handler
